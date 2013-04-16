9ee32a48de83ba0ab22bdc818bc97ef4989b6db5
JN-Jones <jones_h1@web.de>
2013-04-16 12:24:15 +0200

    Umlaute und Link angepasst

--- inc/languages/deutsch_du/admin/menu_suite.lang.php
+++ inc/languages/deutsch_du/admin/menu_suite.lang.php
@@ -1,7 +1,7 @@
 <?php
 //Plugin
-$l['menu_suite'] = "Men&uuml; Suite";
-$l['menu_suite_permissions'] = "Kann die Men&uuml; Suite verwalten";
+$l['menu_suite'] = "Menü Suite";
+$l['menu_suite_permissions'] = "Kann die Menü Suite verwalten";
 $l['menu_suite_no_id'] = "Für diese Aktion ist eine ID vonnöten, diese wurde nicht übermittelt";
 $l['menu_suite_wrong_id'] = "Die übermittelte ID scheint nicht eindeutig zu sein";
 
@@ -18,40 +18,40 @@ $l['ms_title'] = "Titel";
 $l['missing_title'] = "Bitte gib einen Titel an";
 $l['ms_url'] = "Link";
 $l['missing_url'] = "Bitte gib einen Link an";
-$l['no_links'] = "Bisher wurden keine Men&uuml; Punkte eingef&uuml;gt";
-$l['ms_add'] = "Neuen Punkt hinzuf&uuml;gen";
+$l['no_links'] = "Bisher wurden keine Menü Punkte eingefügt";
+$l['ms_add'] = "Neuen Punkt hinzufügen";
 $l['ms_lang'] = "<br /><i>Du kannst entweder einen Namen eingeben oder eine Sprachvariable nutzen (\$lang->)</i>";
 $l['ms_save'] = "Speichern";
-$l['add_success'] = "Men&uuml; Punkt erfolgreich gespeichert";
-$l['ms_delete_confirm'] = "Bist du sicher, dass du diesen Men&uuml; Punkt l&ouml;schen willst?";
-$l['ms_deleted'] = "Men&uuml; Punkt gel&ouml;scht";
-$l['ms_edit'] = "Men&uuml; Punkt bearbeiten";
-$l['edit_success'] = "Men&uuml; Punkt erfolgreich bearbeitet";
+$l['add_success'] = "Menü Punkt erfolgreich gespeichert";
+$l['ms_delete_confirm'] = "Bist du sicher, dass du diesen Menü Punkt löschen willst?";
+$l['ms_deleted'] = "Menü Punkt gelöscht";
+$l['ms_edit'] = "Menü Punkt bearbeiten";
+$l['edit_success'] = "Menü Punkt erfolgreich bearbeitet";
 $l['order'] = "Sortierung";
 $l['ms_order_new'] = "Sortierung aktualisiert";
 
 //Toplinks
-$l['ms_title_desc'] = "Was soll als Men&uuml; Name angezeigt werden?";
+$l['ms_title_desc'] = "Was soll als Menü Name angezeigt werden?";
 $l['ms_link_desc'] = "Gib hier den Namen zu der neuen Seite an";
 $l['ms_img'] = "Bild";
 $l['ms_img_desc'] = "Gib hier den Pfad zu einem Bild ein";
 $l['ms_css'] = "CSS Klassen";
 $l['ms_css_desc'] = "Gib hier weitere Klassen an";
 $l['ms_sid'] = "Designs";
-$l['ms_sid_desc'] = "W&auml;hle hier die Designs unter denen dieser Men&uuml;punkt angezeigt werden soll<br />Wenn keines ausgew&auml;hlt ist wird der Punkt f&uuml;r alle angezeigt";
+$l['ms_sid_desc'] = "Wäier die Designs unter denen dieser Menüpunkt angezeigt werden soll<br />Wenn keines ausgewäst wird der Punkt für alle angezeigt";
 $l['ms_gid'] = "Gruppen";
-$l['ms_gid_desc'] = "W&auml;hle hier die Gruppen, denen dieser Men&uuml;punkt angezeigt werden soll<br />Wenn keine ausgew&auml;hlt ist wird der Punkt allen angezeigt";
+$l['ms_gid_desc'] = "Wäier die Gruppen, denen dieser Menüpunkt angezeigt werden soll<br />Wenn keine ausgewäst wird der Punkt allen angezeigt";
 
 //Styles
 $l['theme'] = "Theme";
 $l['style'] = "Style";
 $l['ms_edit_style'] = "Style bearbeiten";
-$l['ms_style_desc'] = "Wie sollen die Men&uuml;punkte generiert werden (HTML)?<br />Folgende Variablen sind m&ouml;glich:<br />{link}->Der Link des Punktes<br />{img}->Das Bild des Punktes<br />{css}->weitere CSS Klassen des Punktes<br />{name}->Der eigentliche Men&uuml;punkt";
+$l['ms_style_desc'] = "Wie sollen die Menüpunkte generiert werden (HTML)?<br />Folgende Variablen sind möglich:<br />{link}->Der Link des Punktes<br />{img}->Das Bild des Punktes<br />{css}->weitere CSS Klassen des Punktes<br />{name}->Der eigentliche Menüpunkt";
 $l['missing_style'] = "Bitte gib einen Style ein";
 
 //ACP
 $l['ms_acp_title_desc'] = "Was soll als Schnellzugriffname angezeigt werden?";
 $l['ms_acp_url_desc'] = "Gib hier den Link zu dem Modul an (z.B. \"index.php?module=config-menu_suite\")";
-$l['ms_activated_success'] = "{1} Themes erfolgreich zur Verwendung von Men&uuml; Suite aktualisiert";
-$l['ms_activated_failed'] = "Bei {1} Themes gab es Probleme bei der Aktualisierung f&uuml;r Men&uuml; Suite. Wie du diese manuell importierst erf&auml;hrst du <a href=\"http://google.de\">hier</a>";
+$l['ms_activated_success'] = "{1} Themes erfolgreich zur Verwendung von Menü Suite aktualisiert";
+$l['ms_activated_failed'] = "Bei {1} Themes gab es Probleme bei der Aktualisierung für Menü Suite. Wie du diese manuell importierst erfädu <a href=\"http://mybbservice.de/Beitrag-Menü-Suite-Design-hinzufügen\">hier</a>";
 ?>
